ddf864a8905d8b704b47ac467491b76b31548885
UMK <haga5032@naver.com>
2022-11-08 15:28:36 +0900

    반응형 완성

--- js/app.js
+++ js/app.js
@@ -4,19 +4,39 @@ $.fn.hasScrollBar = function () {
     return (this.prop("scrollHeight") == 0 && this.prop("clientHeight") == 0) || (this.prop("scrollHeight") > this.prop("clientHeight"));
 };
 
-// cell default 기본너비 
-var widthlength = 30;
+// cell default  
+let widthlength = 30; // 너비 
 
 //창 로드시 cell크기 조절
 while($("#gridContainer").hasScrollBar()){
-  widthlength =  widthlength - 1
+  widthlength -= 1;
   $("#gridContainer .group .cell").css({"width":widthlength+"%"});
 }
 
 // 화면 크기 변경시 cell 크기 조절
 $( window ).resize(function() {
-    while($("#gridContainer").hasScrollBar()){
-      widthlength =  widthlength - 1
-      $("#gridContainer .group .cell").css({"width":widthlength+"%"});
-    }
+    hasScrollBarfunction();
 });
+
+function hasScrollBarfunction(){
+  while($("#gridContainer").hasScrollBar()){
+    widthlength -= 1;
+    if(widthlength > 0){
+      $("#gridContainer .group .cell").css({"width":widthlength+"%"});
+    }else{
+      break;
+    }
+  }
+ 
+  if(!$("#gridContainer").hasScrollBar()){
+    while(!$("#gridContainer").hasScrollBar()){
+      widthlength += 1;
+      $("#gridContainer .group .cell").css({"width":widthlength+"%"});
+      if($("#gridContainer").hasScrollBar()){
+        widthlength -= 1;
+        $("#gridContainer .group .cell").css({"width":widthlength+"%"});
+        break;
+      }
+    }
+  }
+}
