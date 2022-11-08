8ba90b5b6fd88dc868e34dc5c7813ef2f21414ef
UMK <haga5032@naver.com>
2022-11-08 10:33:34 +0900

    완성

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
+
+//스크롤 생성여부 확인하는 함수
 $.fn.hasScrollBar = function () {
     return (this.prop("scrollHeight") == 0 && this.prop("clientHeight") == 0) || (this.prop("scrollHeight") > this.prop("clientHeight"));
 };
 
-// 스크롤 존재할 경우 제일 하단으로 이동
-if ($("#gridContainer").hasScrollBar()) {
-  console.log("생성됨")
+// cell default 기본너비 
+var widthlength = 30;
+
+//창 로드시 cell크기 조절
+while($("#gridContainer").hasScrollBar()){
+  widthlength =  widthlength - 1
+  $("#gridContainer .group .cell").css({"width":widthlength+"%"});
 }
+
+// 화면 크기 변경시 cell 크기 조절
+$( window ).resize(function() {
+    while($("#gridContainer").hasScrollBar()){
+      widthlength =  widthlength - 1
+      $("#gridContainer .group .cell").css({"width":widthlength+"%"});
+    }
+});
